21f3ff3fd4eaa354a18409a5dba00013052cb383
hamza99113 <alihamza99113@gmail.com>
2023-08-30 01:37:28 +0500

    Counter

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+import Counter from './components/Counter';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <Fragment>
+      <Header/>
+      <Counter />
+    </Fragment>
+  );
+}
+
+export default App;
